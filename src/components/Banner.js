570a430a2dfe88f8e6af99145e934aae4df2874f
Daniel Krook <krook@us.ibm.com>
2021-08-23 10:51:25 -0400

    Add Prettier formatting.
    Signed-off-by: Daniel Krook <krook@us.ibm.com>

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,17 +7,19 @@ const Banner = () => {
 
   return (
     <section className="landing__banner">
-   <div className="bx--grid">
-  <div className="bx--row">
-    <div className="bx--col col__banner__title"><h1 className="BannerTitle" >Pyrrha</h1>
-<h2 className="BannerSubtitle_Responsive">{t("bannerMission")}</h2>
-
-	  </div>
-    <div className="bx--col col__banner__subtitle">  <h2 className="BannerSubtitle">{t("bannerMission")}</h2>
-</div>
-	  </div>
-</div> 
-	  </section>
+      <div className="bx--grid">
+        <div className="bx--row">
+          <div className="bx--col col__banner__title">
+            <h1 className="BannerTitle">Pyrrha</h1>
+            <h2 className="BannerSubtitle_Responsive">{t("bannerMission")}</h2>
+          </div>
+          <div className="bx--col col__banner__subtitle">
+            {" "}
+            <h2 className="BannerSubtitle">{t("bannerMission")}</h2>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
